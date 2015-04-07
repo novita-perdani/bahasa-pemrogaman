2656cc706a1ca2f39612d6d1d1c5acd3c8473c2a
novita.perdani@gmail.com <novita.perdani@gmail.com>
2015-04-07 08:49:02 +0700

    tugas dan latihan

---
+++ pertemuan-6/latihanpertemuan6_arraymultidimensi_14410100181.java
@@ -0,0 +1,104 @@
+import java.lang.System.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+//NOVITA PERDANI - 14410100181
+
+public class latihanpertemuan6_arraymultidimensi_14410100181 {
+	 public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+
+		int [][]data = {
+			{1,0,0,0,0,0},
+			{0,1,0,0,0,0},
+			{0,0,1,0,0,0},
+			{0,0,0,1,0,0},
+			{0,0,0,0,1,1},
+			{0,0,0,0,1,1}
+		};
+				
+		for (int i=0; i<data.length; i++) {
+			for (int j=0; j<data[i].length; j++) { 	
+			System.out.print((data[j][i]==1) ? " # ":" . "); 	
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		
+		int [][]data2 = {
+			{1,0,0,0,0,0},
+			{0,1,0,0,0,0},
+			{0,0,1,0,0,0},
+			{0,0,0,1,0,0},
+			{0,0,0,0,1,1},
+			{0,0,0,0,1,1}
+		};
+		
+		for (int i=0; i<data2.length; i++) { 	
+			for (int j=(data2[i].length-1); j>=0; j--) { 	//case 3
+			System.out.print((data2[j][i]==1) ? " # ":" . ");
+			}
+			System.out.println();
+		}
+		System.out.println()
+		
+		
+		
+		/*-----------------------------------------------------------------------------------------------------------------------------------*/
+		
+		/*
+		int [][]data2 = {
+			{1,1,0,0,0,0},
+			{1,1,0,0,0,0},
+			{0,0,1,0,0,0},
+			{0,0,0,1,0,0},
+			{0,0,0,0,1,0},
+			{0,0,0,0,0,1}
+		};
+		for (int i=0; i<data2.length; i++) {
+			for (int j=0; j<data2[i].length; j++) { 	
+			System.out.print((data2[i][j]==1) ? " # ":" . "); 	// operator ? untuk menentukan kondisi --> "jika benar" : "jika salah"
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		int [][]data3 = {
+			{0,0,0,0,1,1},
+			{0,0,0,0,1,1},
+			{0,0,0,1,0,0},
+			{0,0,1,0,0,0},
+			{0,1,0,0,0,0},
+			{1,0,0,0,0,0}
+		};
+		for (int i=0; i<data3.length; i++) {
+			for (int j=0; j<data3[i].length; j++) {
+			System.out.print((data3[i][j]==1) ? " # ":" . ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		int [][]data4 = {
+			{0,0,0,0,0,1},
+			{0,0,0,0,0,1},
+			{0,0,0,1,0,0},
+			{0,0,1,0,0,0},
+			{1,1,0,0,0,0},
+			{1,1,0,0,0,0}
+		};	
+		for (int i=0; i<data4.length; i++) {
+			for (int j=0; j<data4[i].length; j++) {
+			System.out.print((data4[i][j]==1) ? " # ":" . ");
+			}
+			System.out.println();
+		}
+		*/
+		
+		
+	 }
+}
---
+++ pertemuan-6/tugaspertemuan6_datapenjualan_14410100181.java
@@ -0,0 +1,90 @@
+import java.lang.System.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+//NOVITA PERDANI - 14410100181
+
+public class tugaspertemuan6_datapenjualan_14410100181 {
+	 public static void main(String args[]) {
+
+		int [][]jml_brg = {
+			{5,10,15,20,25,30},
+			{3,6,9,12,15,18},
+			{2,4,6,8,10,12},
+			{1,2,3,4,5,6},
+		};
+		
+		int []hrg_brg 		= {2000,5000,1000,500};
+		String [] barang 	= {"Pensil 2B","Buku Tulis","Penggaris","Penghapus"};
+		int [] perhari 		= new int[6];
+		int sum_barang 		= 0;
+		int tot_harga  		= 0;
+		int persen			= 0;
+		int tot_barang 		= 0;
+		int min				= 999;
+		int max 			= 0;
+
+		/*
+		for (int i=0; i<data.length; i++) {
+			int sum=0;
+			for (int j=0; j<data[i].length; j++) {
+				sum = sum + data[j][i];
+			}
+			//To display the sum
+			System.out.println("The sum is : " +sum);
+		}
+		*/
+		
+		//TOTAL SELURUH BARANG
+		for(int i=0; i<barang.length; i++){
+			for (int j=0; j<jml_brg[i].length; j++) {
+				tot_barang = tot_barang + jml_brg[i][j];
+			}
+		}
+
+		System.out.println("Barang-hari	Senin	Selasa	Rabu	Kamis	Jumat	Sabtu	Hrg per-item	TOT BARANG	TOT HARGA	PROSENTASE	MIN	MAX");
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		 
+		//TAMPILKAN DATA PENJUALAN
+		for(int i=0; i<barang.length; i++){	//baris array --> berdasarkan hari
+			sum_barang 	= 0;
+			min			= 999;
+			max			= 0;
+			System.out.print(barang[i]+"	");	//kolom array --> berdasarkan barang
+			for (int j=0; j<jml_brg[i].length; j++) {
+				System.out.print("	 "+jml_brg[i][j]);
+				perhari[j] = perhari[j] + jml_brg[i][j];
+				sum_barang = sum_barang + jml_brg[i][j];
+				if (jml_brg[i][j]>max) {
+					max = jml_brg[i][j];
+				}
+				if (jml_brg[i][j]<min) {
+					min = jml_brg[i][j];
+				}
+			}
+			System.out.print(" 	"+hrg_brg[i]);
+			System.out.print("		 "+sum_barang);
+			System.out.print("		 "+(sum_barang*hrg_brg[i]));
+			System.out.print("		 "+(100*sum_barang/tot_barang+"%"));
+			System.out.print("		 "+min);
+			System.out.print("	 "+max);
+			System.out.println();
+		}
+		
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		System.out.print("TOTAL PER-HARI : ");
+				
+		//TOTAL PER-HARI
+		for(int i=0; i<perhari.length; i++){
+				System.out.print("	 "+perhari[i]);
+			
+		}
+		
+		System.out.println();
+		//System.out.println("max : " +max);
+		//System.out.println("min : " +min);
+
+
+
+	 }
+}
